c88c667629141e618658ae5c629f8a5ab6f03c1b
Michael Irwin <mikesir87@gmail.com>
2021-09-13 15:19:01 -0400

    Add support for manually-defined k8s deployment
    PoC for a solution of docker/compose-cli#2040. This allows a
    deploy tool to create a ConfigMap that simply serves as a marker.
    It has two labels to provide Compose with its metadata - the
    standard com.docker.compose.project and then a
    com.docker.compose.manual. The tooling finds all Helm releases, as
    well as these custom releases.
    
    Signed-off-by: Michael Irwin <mikesir87@gmail.com>

--- kube/compose.go
+++ kube/compose.go
@@ -193,7 +193,18 @@ func (s *composeService) down(ctx context.Context, projectName string, options a
 
 // List executes the equivalent to a `docker stack ls`
 func (s *composeService) List(ctx context.Context, opts api.ListOptions) ([]api.Stack, error) {
-	return s.sdk.ListReleases()
+	manualDeployments, err := s.client.GetReleases(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	helmStacks, err := s.sdk.ListReleases()
+	if err != nil {
+		return nil, err
+	}
+
+	return append(manualDeployments, helmStacks...), nil
 }
 
 // Build executes the equivalent to a `compose build`
